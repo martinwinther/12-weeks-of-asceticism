4a4bbfbcd31b54fc32bfc24b448ace417bf4fbf1
Martin Winther <martin.winther@protonmail.com>
2025-08-10 16:51:39 +0200

    remove unused components

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = ({ title }) => (
-  <header className="w-full py-6 bg-surface text-primary font-serif text-center text-3xl font-bold border-b border-accent/20">
-    {title}
-  </header>
-);
-
-export default Header; 
